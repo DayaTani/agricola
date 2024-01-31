902852de3af4582ba995279aca4d5d6ad526c5ad
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2024-01-31 15:13:48 +0700

    feat: validate ID card uniqueness

--- src/validate.ts
+++ src/validate.ts
@@ -1,13 +1,15 @@
 import CreateFarmerDto from './types/create-farmer.dto'
+import Farmer from './types/farmer'
 import z from 'zod'
 
 /**
  * Validates a request body for creating a farmer using a Zod schema.
  *
  * @param requestBody - The request body to be validated.
+ * @param farmers - An array of Farmer objects representing the existing list of farmers.
  * @returns Returns a valid CreateFarmerDto if the request body is valid, otherwise returns false.
  */
-const validate = (requestBody: unknown): CreateFarmerDto | false => {
+const validate = (requestBody: unknown, farmers: Farmer[]): CreateFarmerDto | false => {
   /** Zod schema for validating a farmer creation request. */
   const farmerSchema = z.object({
     name: z.string().min(3),
@@ -23,16 +25,43 @@ const validate = (requestBody: unknown): CreateFarmerDto | false => {
 
   /** Represents the validated request body as a CreateFarmerDto object */
   const zodValidRequestBody = result.data as CreateFarmerDto
+  if (!semanticValidateBirthDate(zodValidRequestBody.birthDate)) {
+    return false
+  }
 
-  console.error('through here')
-
-  /** The birthdate as a JavaScript Date object. */
-  const birthdate = new Date(zodValidRequestBody.birthDate)
-  if (isNaN(birthdate.getTime())) {
+  if (!isIdCardUnique(zodValidRequestBody.idCardNumber, farmers)) {
     return false
   }
 
   return zodValidRequestBody
+}
+
+/**
+ * Validates a birthdate represented as a string to ensure it's a valid date.
+ * @param rawBirthDate - The raw birthdate string in YYYY-MM-DD format.
+ * @returns - Returns true if the birthdate is valid, false otherwise.
+ */
+const semanticValidateBirthDate = (rawBirthDate: string): boolean => {
+  /** The birth date as a JavaScript Date object. */
+  const birthDate = new Date(rawBirthDate)
+
+  return !isNaN(birthDate.getTime())
+}
+
+/**
+ * Checks if an ID card number is unique among a list of farmers.
+ * @param idCardNumber - The ID card number to check for uniqueness.
+ * @param farmers - An array of Farmer objects representing the list of farmers.
+ * @returns - Returns true if the ID card number is unique among the farmers, false otherwise.
+ */
+const isIdCardUnique = (idCardNumber: string, farmers: Farmer[]): boolean => {
+  for (const farmer of farmers) {
+    if (idCardNumber === farmer.idCardNumber) {
+      return false
+    }
+  }
+
+  return true
 }
 
 export default validate
--- tests/validate.test.ts
+++ tests/validate.test.ts
@@ -1,13 +1,22 @@
 import CreateFarmerDto from '../src/types/create-farmer.dto'
+import Farmer from '../src/types/farmer'
 import validate from '../src/validate'
 
 describe('validate function', () => {
+  const farmers: Farmer[] = [
+    { id: 1, name: 'Humphrey Bogart', idCardNumber: '1234567890', birthDate: '1899-12-25' },
+    { id: 2, name: 'Katharine Hepburn', idCardNumber: '2345678901', birthDate: '1907-05-12' },
+    { id: 3, name: 'Cary Grant', idCardNumber: '3456789012', birthDate: '1904-01-18' },
+    { id: 4, name: 'Ingrid Bergman', idCardNumber: '4567890123', birthDate: '1915-08-29' },
+    { id: 5, name: 'James Stewart', idCardNumber: '5678901234', birthDate: '1908-05-20' },
+  ]
+
   test.each([
     { name: 'James Stewart', idCardNumber: '0123456789', birthDate: '1908-05-20' },
     { name: 'Miriam Hopkins', idCardNumber: '0345678901', birthDate: '1902-10-18' },
     { name: 'Humphrey Bogart', idCardNumber: '0456789012', birthDate: '1899-12-25', extra: 'value' },
   ])('should return a valid CreateFarmerDto for valid input', requestBody => {
-    const result = validate(requestBody)
+    const result = validate(requestBody, farmers)
 
     expect(result).toEqual<CreateFarmerDto>({
       name: requestBody.name,
@@ -26,7 +35,7 @@ describe('validate function', () => {
     { name: 'Humphrey Bogart', idCardNumber: 123456789, birthDate: false },
   ])('should return false for invalid or missing fields', requestBody => {
 
-    const result = validate(requestBody)
+    const result = validate(requestBody, farmers)
 
     expect(result).toBe(false)
   })
@@ -42,7 +51,25 @@ describe('validate function', () => {
       birthDate,
     }
 
-    const result = validate(requestBody)
+    const result = validate(requestBody, farmers)
+
+    expect(result).toBe(false)
+  })
+
+  test.each([
+    '1234567890',
+    '2345678901',
+    '3456789012',
+    '4567890123',
+    '5678901234',
+  ])('should return false for existing ID card number', idCardNumber => {
+    const requestBody = {
+      name: 'Jane Doe',
+      idCardNumber,
+      birthDate: '1915-01-34',
+    }
+
+    const result = validate(requestBody, farmers)
 
     expect(result).toBe(false)
   })
